f22ac44ef3c3ec672e6fa8cce63e4521c595bc4e
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-06 07:22:46 -0600

    Add buttons and diner name label

--- DinerMenu/DinerMenu.Designer.vb
+++ DinerMenu/DinerMenu.Designer.vb
@@ -22,10 +22,80 @@ Partial Class DinerMenu
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Location = New System.Drawing.Point(421, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(236, 25)
+        Me.DinerNameLabel.TabIndex = 0
+        Me.DinerNameLabel.Text = "Insert Diner Name Here"
+        Me.DinerNameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(911, 599)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(198, 157)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Daily Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(693, 599)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(198, 157)
+        Me.SaladButton.TabIndex = 3
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'SoupButton
+        '
+        Me.SoupButton.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.SoupButton.Location = New System.Drawing.Point(474, 599)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(198, 157)
+        Me.SoupButton.TabIndex = 4
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(29, 726)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(83, 29)
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DinerMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1118, 769)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Name = "DinerMenu"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents FishButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
--- DinerMenu/DinerMenu.vb
+++ DinerMenu/DinerMenu.vb
@@ -5,6 +5,12 @@
 'https://github.com/heinelli/DinerMenu.git
 
 Public Class DinerMenu
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles DinerNameLabel.Click
 
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+
+    End Sub
 End Class
 
